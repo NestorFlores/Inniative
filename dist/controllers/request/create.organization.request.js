a5d6b82939aae3ada29f91b8293c47cd1ec0aa7b
NestorFlores <n.flores@spds.info>
2019-01-25 13:13:01 -0300

    Deleted binary files

--- dist/controllers/request/create.organization.request.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CreateOrganizationRequest {
-}
-exports.CreateOrganizationRequest = CreateOrganizationRequest;
-//# sourceMappingURL=create.organization.request.js.map
--- dist/controllers/request/create.user.request.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CreateUserRequest {
-}
-exports.CreateUserRequest = CreateUserRequest;
-//# sourceMappingURL=create.user.request.js.map
